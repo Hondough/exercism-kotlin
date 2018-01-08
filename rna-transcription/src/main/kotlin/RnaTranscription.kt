462bbd3ed7e93a90eda8e837f5374bfe0d8c5424
John Hannemann <Hondough@users.noreply.github.com>
2018-01-08 09:11:26 -0600

    Passed all 5 tests

--- rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,10 @@
-fun transcribeToRna(dna: String): String = ""
+val rnaToDna: Map<Char, Char> = hashMapOf(
+    'C' to 'G',
+    'G' to 'C',
+    'T' to 'A',
+    'A' to 'U'
+)
+
+fun transcribeToRna(dna: String): String {
+	return dna.map { rnaToDna.get(it) } .joinToString(separator = "")
+}
