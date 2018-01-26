f8bf5bb288ecd077fb83c170b7ddf62f1e334eb6
John Hannemann <Hondough@users.noreply.github.com>
2018-01-26 09:29:41 -0600

    Passes all 15 tests

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,5 +1,9 @@
 class Hamming {
     companion object {
-        fun compute(left: String, right: String) = 0
+        fun compute(left: String, right: String) =
+            if (left.length != right.length)
+                throw IllegalArgumentException("left and right strands must be of equal length.")
+            else
+                left.zip(right, {l, r -> if (l == r) 0 else 1}).sum()
     }
 }
