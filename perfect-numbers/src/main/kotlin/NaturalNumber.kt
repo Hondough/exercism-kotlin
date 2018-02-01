a3c8e5b9e39e6b8f1235a70c9bf92e8e0e32d120
John Hannemann <Hondough@users.noreply.github.com>
2018-01-31 22:48:00 -0600

    Passed all 13 tests

--- perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -3,6 +3,13 @@ enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
-fun classify(naturalNumber: Int): Classification {
-    return Classification.DEFICIENT
-}
+fun Int.isNotNaturalNumber(): Boolean = this < 1
+fun Int.aliquotSum(): Int = (1 until this).filter { f -> this % f == 0 } .sum()
+
+fun classify(naturalNumber: Int): Classification =
+    when {
+        naturalNumber.isNotNaturalNumber()         -> throw IllegalArgumentException()
+        naturalNumber.aliquotSum() < naturalNumber -> Classification.DEFICIENT
+        naturalNumber.aliquotSum() > naturalNumber -> Classification.ABUNDANT
+        else                                       -> Classification.PERFECT
+    }
