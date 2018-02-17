fc105ae51d27148fe8785f137e57442ebdd3f4a3
John Hannemann <Hondough@users.noreply.github.com>
2018-02-17 13:11:21 -0600

    Used buildSequence

--- nth-prime/src/main/kotlin/Prime.kt
+++ nth-prime/src/main/kotlin/Prime.kt
@@ -1,31 +1,19 @@
 import kotlin.math.ln
+import kotlin.coroutines.experimental.buildSequence
 
 object Prime {
 
     fun Int.max(that: Int) = if (this > that) this else that
+    fun Int.sqrt() = Math.sqrt(this.toDouble()).toInt()
 
     fun nth(n: Int): Int {
         require(n > 0) { "There is no zeroth prime." }
-
-        val nd = n.toDouble()
-        val limit = 11.max((nd * (ln(nd) + ln(ln(nd)))).toInt())
-        var primes = (2 .. limit).toList()
-
-        primes.forEach { primes -= ((it + it) .. limit step it).toList() }
-
-        return primes.get(n - 1)
+        return primes.elementAt(n - 1)
     }
 
-
-
-    // fun primes() = buildSequence {
-    //     var terms = Pair(2, 3)
-
-    //     // this sequence is infinite
-    //     while(true) {
-    //         val fac = (terms.first - 1) * terms.second
-    //         if ((fac + 1) % terms.first == 0) yield(terms.first)
-    //         terms = Pair(terms.first + 1, fac)
-    //     }
-    // }
+    val primes: Sequence<Int> = buildSequence {
+        yield(2)
+        yieldAll(generateSequence(3, { it + 2 })
+            .filterNot { value -> (3 .. value.sqrt() step 2).any { value % it == 0 } } )
+    }
 }
