a33cf3f2be9a2e719ce048ee036e50cabf135dd2
John Hannemann <Hondough@users.noreply.github.com>
2018-02-18 11:43:46 -0600

    Changed constructor to accept any Number type.

--- triangle/src/main/kotlin/Triangle.kt
+++ triangle/src/main/kotlin/Triangle.kt
@@ -1,14 +1,16 @@
-class Triangle (val a: Double, val b: Double, val c: Double) {
-    constructor (a: Int, b: Int, c: Int) : this(a.toDouble(), b.toDouble(), c.toDouble())
+class Triangle (val x: Number, val y: Number, val z: Number) {
 
     init {
+        val a = x.toDouble()
+        val b = y.toDouble()
+        val c = z.toDouble()
         require(a + b + c != 0.0)
-        require(a + b > c && b + c > a)
+        require(a + b > c && b + c > a && a + c > b)
     }
 
-    val isEquilateral = a == b && a == c
+    val isEquilateral = x == y && x == z
 
-    val isIsosceles = a == b || a == c || b == c
+    val isIsosceles = x == y || x == z || y == z
 
     val isScalene = !isIsosceles
 }
