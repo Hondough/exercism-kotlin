5a47479ffb5c81344d2cbcd7dcaaf4cf02703f63
John Hannemann <Hondough@users.noreply.github.com>
2018-02-18 22:56:53 -0600

    Passes all 5 tests

--- diamond/src/main/kotlin/DiamondPrinter.kt
+++ diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,5 +1,19 @@
 class DiamondPrinter {
 
+    val alphabet = ('A' .. 'Z')
 
+    // val blend: (ReversableAndDropable) -> ReversableAndDropable = { half -> half + half.reversed().drop(1) }
 
+    fun printToList(char: Char): List<String> {
+        val chars = ('A' .. char)
+        val len = chars.count()
+
+        val formatLine: (Int, Char) -> String = { i, c ->
+            val half = " ".repeat(len - (i + 1)) + c + " ".repeat(i)
+            half + half.reversed().drop(1)
+        }
+
+        val half = chars.mapIndexed(formatLine)
+        return half + half.reversed().drop(1)
+    }
 }
