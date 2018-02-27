2be156833bbad8834a4654ed431996fc03325ca6
John Hannemann <Hondough@users.noreply.github.com>
2018-02-26 22:25:22 -0600

    Removed dead code

--- flatten-array/src/main/kotlin/Flattener.kt
+++ flatten-array/src/main/kotlin/Flattener.kt
@@ -1,16 +1,5 @@
 object Flattener {
 
-    fun flatten1(a: List<Any?>): List<Any> {
-        val result = ArrayList<Any>()
-        for (item in a) {
-            when (item) {
-                is Int -> result.add(item)
-                else -> item?.let { result.addAll(flatten(it as List<Any>)) }
-            }
-        }
-        return result
-    }
-
     fun flatten(a: List<Any?>): List<Any> =
         a.fold(emptyList<Any>()) { acc, i ->
             when (i) {
