11478107945bc1feec71372680f881d464015601
John Hannemann <Hondough@users.noreply.github.com>
2019-02-21 14:17:36 -0600

    Refactored community solution

---
+++ spiral-matrix/src/main/kotlin/SpiralMatrix.1.kt
@@ -0,0 +1,60 @@
+// import kotlin.coroutines.experimental.buildSequence
+
+// class SpiralMatrix (val spiralMatrix: Array<IntArray>, val position: Position) {
+
+//     constructor (n: Int): this(Array(n, { IntArray(n) }), Position.Start())
+
+//     fun applyMove(direction: Direction, value: Int): SpiralMatrix = SpiralMatrix (insertValue(direction, value), nextPosition(direction))
+   
+//     fun nextPosition(direction: Direction): Position.RC =
+//         when (position) {
+//             is Position.RC ->
+//                 when (direction) {
+//                     is Direction.Right  -> Position.RC(position.row, position.col + 1)
+//                     is Direction.Down   -> Position.RC(position.row + 1, position.col)
+//                     is Direction.Left   -> Position.RC(position.row, position.col - 1)
+//                     is Direction.Up     -> Position.RC(position.row - 1, position.col)
+//                 }
+//             is Position.Start -> Position.RC(0,0)
+//         }
+    
+//     fun insertValue(direction: Direction, value: Int): Array<IntArray> {
+//         val nextPosition = nextPosition(direction)
+//         spiralMatrix[nextPosition.row][nextPosition.col] = value
+//         return spiralMatrix
+//     }
+
+//     sealed class Position {
+//         class RC(val row: Int, val col: Int): Position()
+//         class Start(): Position()
+//     }
+
+//     sealed class Direction {
+//         class Right(): Direction()
+//         class Down(): Direction()
+//         class Left(): Direction()
+//         class Up(): Direction()
+//     }
+
+//     companion object {
+
+//         fun <T> Sequence<T>.repeat() = buildSequence { while (true) yieldAll(this@repeat) }
+//         fun <T> T.repeat(n: Int): Sequence<T> = buildSequence { repeat(n) { yield(this@repeat) } }
+
+//         val directions: Sequence<Direction> = listOf(Direction.Right(), Direction.Down(), Direction.Left(), Direction.Up()).asSequence()
+
+//         fun repeats(n: Int): Sequence<Int> = 
+//             generateSequence(Pair(n * 2 - 1, n), { 
+//                 if (it.second > 0) Pair(it.first - 1, it.first / 2) else null 
+//             }) .map { it.second }
+
+//         fun ofSize(n: Int): Array<IntArray> {
+    
+//            val moves: Sequence<Direction> = repeats(n).zip(directions.repeat(), { r, d -> d.repeat(r) }).flatten();
+
+//            val finalState = moves.foldIndexed(SpiralMatrix(n), { index, spiralMatrix, direction -> spiralMatrix.applyMove(direction, index + 1) })
+
+//            return finalState.spiralMatrix
+//         }
+//     }
+// }
--- spiral-matrix/src/main/kotlin/SpiralMatrix.kt
+++ spiral-matrix/src/main/kotlin/SpiralMatrix.kt
@@ -1,60 +1,17 @@
-import kotlin.coroutines.experimental.buildSequence
+package SpiralMatrix
 
-class SpiralMatrix (val spiralMatrix: Array<IntArray>, val position: Position) {
-
-    constructor (n: Int): this(Array(n, { IntArray(n) }), Position.Start())
-
-    fun applyMove(direction: Direction, value: Int): SpiralMatrix = SpiralMatrix (insertValue(direction, value), nextPosition(direction))
-   
-    fun nextPosition(direction: Direction): Position.RC =
-        when (position) {
-            is Position.RC ->
-                when (direction) {
-                    is Direction.Right  -> Position.RC(position.row, position.col + 1)
-                    is Direction.Down   -> Position.RC(position.row + 1, position.col)
-                    is Direction.Left   -> Position.RC(position.row, position.col - 1)
-                    is Direction.Up     -> Position.RC(position.row - 1, position.col)
-                }
-            is Position.Start -> Position.RC(0,0)
-        }
-    
-    fun insertValue(direction: Direction, value: Int): Array<IntArray> {
-        val nextPosition = nextPosition(direction)
-        spiralMatrix[nextPosition.row][nextPosition.col] = value
-        return spiralMatrix
-    }
-
-    sealed class Position {
-        class RC(val row: Int, val col: Int): Position()
-        class Start(): Position()
-    }
-
-    sealed class Direction {
-        class Right(): Direction()
-        class Down(): Direction()
-        class Left(): Direction()
-        class Up(): Direction()
-    }
-
-    companion object {
-
-        fun <T> Sequence<T>.repeat() = buildSequence { while (true) yieldAll(this@repeat) }
-        fun <T> T.repeat(n: Int): Sequence<T> = buildSequence { repeat(n) { yield(this@repeat) } }
-
-        val directions: Sequence<Direction> = listOf(Direction.Right(), Direction.Down(), Direction.Left(), Direction.Up()).asSequence()
-
-        fun repeats(n: Int): Sequence<Int> = 
-            generateSequence(Pair(n * 2 - 1, n), { 
-                if (it.second > 0) Pair(it.first - 1, it.first / 2) else null 
-            }) .map { it.second }
-
-        fun ofSize(n: Int): Array<IntArray> {
-    
-           val moves: Sequence<Direction> = repeats(n).zip(directions.repeat(), { r, d -> d.repeat(r) }).flatten();
-
-           val finalState = moves.foldIndexed(SpiralMatrix(n), { index, spiralMatrix, direction -> spiralMatrix.applyMove(direction, index + 1) })
-
-           return finalState.spiralMatrix
+fun ofSize(n: Int): Array<IntArray> {
+    val matrix = Array(n, { IntArray(n) })
+    fun fill(v: Int, ru: Int, rd: Int, cl: Int, cr: Int): Unit {
+        if (!(ru >= rd || cl >= cr)) {
+            var value = v
+            (cl until cr).forEach { matrix[ru][it] = value++ }
+            (ru + 1 until rd).forEach { matrix[it][cr - 1] = value++ }
+            (cr - 2 downTo cl).forEach { matrix[rd - 1][it] = value++ }
+            (rd - 2 downTo ru + 1).forEach { matrix[it][cl] = value++ }
+            fill(value, ru + 1, rd - 1, cl + 1, cr - 1)
         }
     }
+    fill(1, 0, n, 0, n)
+    return matrix
 }
